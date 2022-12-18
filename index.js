5fd54908bc58df10de901d5e4e8982b32609adde
janfabian <mail@janfabian.cz>
2022-12-18 08:25:45 +0100

    time

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ let fn = require(path.resolve(dir, "./index.js"));
 if (typeof fn !== "function") {
   fn = fn[args[1]];
 }
+const start = new Date().getTime();
 console.log(fn(inputData));
+if (process.env.TIME) {
+  console.log(new Date().getTime() - start, "ms");
+}
